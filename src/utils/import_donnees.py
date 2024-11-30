2f4c28f9bed1dc295aaa89fe6a8223bfbcc24222
Titouan Beauhaire <titouan.beauhaire@etudiant.univ-lr.fr>
2024-11-30 17:14:33 +0100

    fichier housing.csv dans data

--- src/utils/import_donnees.py
+++ src/utils/import_donnees.py
@@ -3,10 +3,11 @@ import pandas as pd
 
 def load_data():
     # Télécharger la dernière version du dataset
-    path = kagglehub.dataset_download("sukhmandeepsinghbrar/housing-price-dataset")
-    print(f"Données téléchargées : {path}")
+    # path = kagglehub.dataset_download("sukhmandeepsinghbrar/housing-price-dataset")
+    # print(f"Données téléchargées : {path}")
 
     # Construire le chemin vers le fichier CSV
+    path = "data"
     csv_path = f"{path}/Housing.csv"
 
     # Lire le fichier CSV avec pandas
